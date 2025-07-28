6feaecc158c927f568b2b64debe17040b0735c0f
Mansurion <mansoor.kumratov@mail.ru>
2025-07-28 23:44:53 +0300

    lesson 2

--- core.js
+++ core.js
@@ -3,5 +3,12 @@
 
 let message;
 message = 'Hello!';
-
 alert(message);
+
+let admin, name;
+name = "Mans";
+admin = name;
+alerty(admin);
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
