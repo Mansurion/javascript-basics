30b9167df6592146452d0f832e5a301486d4cf59
Mansurion <mansoor.kumratov@mail.ru>
2025-08-13 23:44:11 +0300

    username & pass

--- core.js
+++ core.js
@@ -1,7 +1,19 @@
-alert( 5 > 4 );
-alert( "ананас" > "яблоко" );
-alert( "2" > "12" );
-alert( undefined == null );
-alert( undefined === null );
-alert( null == "\n0\n" );
-alert( null === +"\n0\n" );
+let login = prompt('введите логин');
+
+if (login === 'mans') {
+
+  let password = prompt('введите пароль');
+
+  if (password === 'qwerty') {
+    alert('вы вошли!');
+  } else if (password === '' || password === null) {
+    alert('Отменено');
+  } else {
+    alert( 'Неверный пароль' );
+  }
+
+} else if (login === '' || login === null) {
+  alert('Отменено');
+} else {
+  alert('неверный логин');
+}
