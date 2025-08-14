f4f86d8a66b82cb1217f9b48e2f8726bb98b3eee
Mansurion <mansoor.kumratov@mail.ru>
2025-08-14 20:32:39 +0300

    lesson-7.2

--- core.js
+++ core.js
@@ -1,9 +1,11 @@
-let company = prompt('Какое «официальное» название JavaScript?')
+let number = prompt('Введите число от одного до трех')
 
-if (company == 'Netscape') {
-  alert('Верно')
+if (number > 0) {
+  alert('1');
 }
-
+else if (number < 0) {
+  alert('-1')
+}
 else {
-  alert('Неправильно')
+  alert('0')
 }
