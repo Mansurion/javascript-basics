b02908eb943d10ccc67aca84a9d17522c6ddf475
Mansurion <mansoor.kumratov@mail.ru>
2025-08-19 15:00:53 +0300

    userName&password-1.01

--- core.js
+++ core.js
@@ -1,11 +1,22 @@
-let number = prompt('Введите число от одного до трех')
+const login = prompt('Введите логин');
+const password = prompt('Введите пароль');
 
-if (number > 0) {
-  alert('1');
+const users = [
+  {name: 'Mansur', login: 'mans', password: '1122'},
+  {name: 'Madina', login: 'madi', password: '3344'}
+]
+
+let userName = 0
+
+for (let i = 0; i < users.length; i++) {
+  const user = users[i];
+  if (login === user.login && password === user.password) {
+      userName = user.name
+  }
 }
-else if (number < 0) {
-  alert('-1')
-}
-else {
-  alert('0')
+
+if (userName) {
+    alert("Вы вошли, " + userName)
+} else {
+    alert("Неправильный логин или пароль")
 }
